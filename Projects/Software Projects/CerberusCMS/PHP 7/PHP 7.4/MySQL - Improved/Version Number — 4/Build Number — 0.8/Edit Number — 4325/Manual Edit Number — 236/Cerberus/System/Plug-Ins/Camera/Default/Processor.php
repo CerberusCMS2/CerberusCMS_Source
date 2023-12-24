1571e270e262799929f4e81f5033cf6435e4ebaa
Cerberus Content Management System <154483627+CerberusCMS2@users.noreply.github.com>
2023-12-24 14:50:19 -0800

    Add files via upload

--- Projects/Software Projects/CerberusCMS/PHP 7/PHP 7.4/MySQL - Improved/Version Number — 4/Build Number — 0.8/Edit Number — 4325/Manual Edit Number — 236/Cerberus/System/Plug-Ins/Camera/Default/Processor.php
+++ Projects/Software Projects/CerberusCMS/PHP 7/PHP 7.4/MySQL - Improved/Version Number — 4/Build Number — 0.8/Edit Number — 4325/Manual Edit Number — 236/Cerberus/System/Plug-Ins/Camera/Default/Processor.php
@@ -2,5 +2,5 @@
 
 var_dump($_FILES);
 
-move_uploaded_file($_FILES['blobFile']['tmp_name'], 'Member/streamed_video.mp4');
+move_uploaded_file($_FILES['blobFile']['tmp_name'], './Member/$_GLOBAL_MEMBER_USERNAME/Files/Video/Live/streamed_video.mp4');
 ?>
