bc6cbf1433b04aae58bf4963ad691dba47b6d62e
Cerberus Content Management System <154483627+CerberusCMS2@users.noreply.github.com>
2023-12-27 13:33:01 -0800

    Add files via upload

--- Projects/Software Projects/CerberusCMS/PHP 7/PHP 7.4/MySQL - Improved/Version Number — 4/Build Number — 0.8/Edit Number — 4325/Manual Edit Number — 236/Cerberus/System/Plug-Ins/Camera/Default/Live/Panel/Camera.php
+++ Projects/Software Projects/CerberusCMS/PHP 7/PHP 7.4/MySQL - Improved/Version Number — 4/Build Number — 0.8/Edit Number — 4325/Manual Edit Number — 236/Cerberus/System/Plug-Ins/Camera/Default/Live/Panel/Camera.php
@@ -64,14 +64,14 @@
  ============================================================================================================
 */
 echo ("
-			<VIDEO AUTOPLAY=\"\" ID=\"stream-element\" CONTROLS WIDTH=\"256\" HEIGHT=\"144\">
+			<VIDEO AUTOPLAY=\"\" ID=\"stream-element\" CONTROLS WIDTH=\"125\" HEIGHT=\"125\">
 				<SOURCE SRC=\"\" TYPE=\"\">
 			</VIDEO><BR><BR>
 
 			Options :: Controls :: Live Video
 			<HR ALIGN=\"LEFT\" WIDTH=\"50%\">
 				<SELECT ID=\"CameraSource\"></SELECT><BR>
-				<BUTTON ID=\"stream-start\">Record Video Stream</BUTTON>
+				<BUTTON ID=\"stream-start\">Record Video Stream</BUTTON><BR>
 				<BUTTON ID=\"stream-stop\">Stop Video Stream</BUTTON>
 
 			<SCRIPT SRC=\"Camera.js\"></SCRIPT>
